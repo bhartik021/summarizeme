e016b401a16773b8923096cf79140f136afdbae0
Bharti Kumari <bhartik021@gmail.com>
2023-04-23 17:18:09 +0530

    Building the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+/**
+ * Import statements for the Hero and Demo components.
+ * These components are used to display the main content of the page.
+ * @import {Hero} from './components/Hero'
+ * @import {Demo} from './components/Demo'
+ */
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+
+/**
+ * This statement imports the CSS file for the App component. The CSS file contains
+ * styles that are applied to the App component and its child components. The styles
+ * are defined using CSS syntax and are used to control the layout and appearance of
+ * the components. The imported CSS file is applied to the App component using the
+ * className attribute in the JSX code.
+ */
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+        <div className="main">
+            <div className="gradient"/>
+        </div>
+
+        <div className="app">
+            <Hero/>
+            <Demo/>
+        </div>
+    </main>
   )
 }
 
